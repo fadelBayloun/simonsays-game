6b57020b3650ca4b21460805e3dcd3b6832b5558
fadelBayloun <fadel.bayloun12@gmail.com>
2023-11-11 19:17:23 +0200

    implemented some game features

--- game.js
+++ game.js
@@ -2,9 +2,13 @@ let green = document.getElementById("green");
 let red = document.getElementById("red");
 let blue = document.getElementById("blue");
 let yellow = document.getElementById("yellow");
+let title = document.getElementById("level-title");
+
 let colors = [green, red, blue, yellow];
 let gameStarted = false;
-let level;
+let level, random, isCorrect;
+let playerInput = [];
+let sequence = [];
 
 // starting the game
 
@@ -12,21 +16,60 @@ document.addEventListener("keydown", (event) => {
   if (!gameStarted) startGame();
 });
 
-async function processStep(step) {
-  step.classList.add("pressed");
-  await new Promise((resolve) => setTimeout(resolve, 800));
-  step.classList.remove("pressed");
+function startGame() {
+  gameStarted = true;
+  playerInput = [];
+  sequence = [];
+  level = 1;
+  startNewRound();
 }
 
-async function startGame() {
-  gameStarted = true;
-  level = 1;
-  let answers = [];
-  let random = Math.floor(Math.random() * 4);
-  sequence += colors[random];
-  for (let i = 0; i < sequence.length; i++) {
-    await processStep(sequence[i]);
+function startNewRound() {
+  title.innerHTML = `Level ${level}`;
+  generateSequence();
+  colorEvent(sequence[sequence.length - 1], "mousedown");
+  setTimeout(() => {
+    colorEvent(sequence[sequence.length - 1], "mouseup");
+  }, 500);
+  for (const color of colors) {
+    color.onclick = (event) => {
+      playerInput.push(color);
+      isCorrect = handlePlayerInput();
+    };
+    if (!isCorrect) {
+      gameOver();
+    } else if (playerInput.length === sequence.length) {
+      level++;
+      startNewRound();
+    }
   }
+}
+
+function gameOver() {
+  title.innerHTML = `GameOver!`;
+  gameStarted = false;
+}
+
+function handlePlayerInput() {
+  if (playerInput.length > sequence.length) {
+    return false;
+  }
+  if (playerInput.length === sequence.length) {
+    for (let i = 0; i < playerInput.length; i++) {
+      if (playerInput[i] !== sequence[i]) {
+        return false;
+      }
+    }
+    // Player completed the sequence correctly
+    return true;
+  }
+  // Player's input is shorter than the correct sequence, continue!
+  return true;
+}
+
+function generateSequence() {
+  random = Math.floor(Math.random() * 4);
+  sequence.push(colors[random]);
 }
 
 // adding press effect for colors
